678b1214344d043e7abf37533aaaccf644633a29
unknown <preetambb812@gmail.com>
2024-12-26 23:25:01 +0530

    2nd commit

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        int age=12;
+        if (age<=18) {
+            System.out.println("NA");
+        }
+        else{
+            System.out.println("Eligible");
+        }
     }
 }
