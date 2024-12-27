7bed7e79305dafa815abc3b6651d181f5fb0d824
unknown <preetambb812@gmail.com>
2024-12-27 13:18:28 +0530

    third commit

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
 
     public static void main(String[] args){
-        int age=12;
+        int age=807;
         if (age<=18) {
             System.out.println("NA");
         }
-        else{
+        else if(age>=18 && age<=99) {
             System.out.println("Eligible");
         }
+        else {
+            System.out.println("default");
+        }
+        
     }
 }
