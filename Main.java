4882679ee8f073f4e40918ddc5019638209be5a8
unknown <preetambb812@gmail.com>
2024-12-30 22:08:10 +0530

    3rd commit

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 public class Main {
 
     public static void main(String[] args){
-        int age=807;
-        if (age<=18) {
-            System.out.println("NA");
+        try{
+            int balance = 5000;
+            int withdrawal = 6000;
+
+            if(withdrawal>balance){
+                throw new Exception("insufficient fund");
+            }
+            System.out.println("succes");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
-        else if(age>=18 && age<=99) {
-            System.out.println("Eligible");
-        }
-        else {
-            System.out.println("default");
-        }
-        
-    }
+   
+}
 }
