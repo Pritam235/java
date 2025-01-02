78d043da62c9ba704df354040cc7a99a0fa5b1e8
unknown <preetambb812@gmail.com>
2025-01-02 21:31:07 +0530

    vote

---
+++ Basic/Vote.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Vote {
+    public static void main (String [] args){
+        int a=14;
+        if (a<=18){
+        System.out.println("not vote");
+        }
+        else if (a>=18 && a<=90){
+            System.out.println("vote");
+        }
+else{
+
+    System.out.println("dead");
+}
+
+    }
+     
+}
