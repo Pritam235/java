8e23da9c114b675283aac5c258a824499497decb
unknown <preetambb812@gmail.com>
2025-01-06 23:04:46 +0530

    first commit

---
+++ Basic/switch.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class switch {
+    
+    public static void Main(String[] args){
+
+    Scanner scan= new Scanner(System.in);
+
+    switch(button){
+        case 1:
+        System.out.println("namasthe");
+        break;
+        case 2:
+        System.out.println("Hello");
+        break;
+        case 3:
+        System.out.println("bonjour");
+        break;
+        default:
+
+        System.out.println("invalid");
+
+    
+    }
+}
+}
