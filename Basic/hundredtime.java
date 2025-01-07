97a7e2318a6bfbabc52bb36c669ba446b85b9f33
unknown <preetambb812@gmail.com>
2025-01-07 18:02:30 +0530

    first commit

---
+++ Basic/hundredtime.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class hundredtime {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=100;i++){
+            System.out.println("Hello world");
+        }
+    }
+    
+}
